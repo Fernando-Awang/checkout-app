84055e49da5ae6bd9ef12f1c0f5d3ba304561656
Fernando-Awang <fernandoawang@gmail.com>
2023-03-29 19:43:45 +0700

    update : layout.blade.php

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -25,7 +25,63 @@
     </nav>
 
     @yield('content')
-    
+
+    @stack('script')
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script>
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 2500,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+
+        function ShowToast(iconToast = 'success', msg = 'Toast Success') {
+            return Toast.fire({
+                icon: iconToast,
+                title: msg
+            })
+        }
+        function ShowModal(iconModal = 'success', msg = 'Modal Success', title=  '') {
+            return Swal.fire(
+                title,
+                msg,
+                iconModal
+            )
+        }
+        function SuccessToast(msg = 'Berhasil') {
+            return ShowToast('success', msg)
+        }
+        function ErrorToast(msg = 'Terjadi Error') {
+            return ShowToast('error', msg)
+        }
+        function SuccessModal(msg = 'Berhasil') {
+            return ShowModal('success', msg)
+        }
+        function ErrorModal(msg = 'Terjadi Error') {
+            return ShowModal('error', msg)
+        }
+    </script>
+
+    @if (session('error'))
+    <script>
+        ErrorModal('{{session()->get("error")}}')
+    </script>
+    @endif
+
+    @if (session('success'))
+    <script>
+        SuccessModal('{{session()->get("success")}}')
+    </script>
+    @endif
+
   </body>
 </html>
