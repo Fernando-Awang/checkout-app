b81a2f974181e06e6704f977293625768b90b508
Fernando-Awang <fernandoawang@gmail.com>
2023-03-29 19:10:30 +0700

    update : helpers

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -9,3 +9,32 @@ function printError($th)
     ];
 }
 
+function userId()
+{
+    return auth()->user()->id;
+}
+
+function sendResponseJson($success, $msg='Terjadi Kesalahan, Buka Log Error', $data = null, $code=200)
+{
+    return response()->json([
+        'success' => $success,
+        'message' => $msg,
+        'data' => $data,
+    ], $code);
+}
+
+function formatRupiah($val)
+{
+    return 'Rp. ' . number_format($val, 0, ',', '.');
+}
+
+function generateRandomString($length)
+{
+    $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
