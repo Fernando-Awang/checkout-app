272057ccd79d20f5d1030ef5886b56517eed480a
Fernando-Awang <fernandoawang@gmail.com>
2023-03-29 19:05:57 +0700

    update : helpers

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,2 +1,11 @@
 <?php
 
+function printError($th)
+{
+    return (object)[
+        'message' => $th->getMessage(),
+        'file' => $th->getFile(),
+        'line' => $th->getline(),
+    ];
+}
+
